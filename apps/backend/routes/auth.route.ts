4d1ab1c49cd1752fd449bc6d952d89ff2ad5f892
MohakGupta2004 <mohakgupta500@gmail.com>
2026-02-15 12:15:43 +0530

    fix: auth routes modified and add

--- apps/backend/routes/auth.route.ts
+++ apps/backend/routes/auth.route.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import { AuthController } from '../controllers/auth.controller.ts';
+import express from "express";
+import { AuthController } from "../controllers/auth.controller.ts";
 
 const router = express.Router();
 
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
-
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
+router.post("/generate-otp", AuthController.generateOtp);
+router.post("/veritfy-otp", AuthController.verifyOtp);
+router.post("/change-password", AuthController.changePassword);
 export default router;
